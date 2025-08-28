9bad92297b8c25127c997f351efb548b837df75e
Islam Atwa <islamatwa429@gmail.com.com>
2025-08-29 00:54:54 +0300

    create store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../features/cart/cartSlice';
+import cartReducer  from '../features/cart/cartSlice';
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer, // Assign our cart slice reducer to the 'cart' key in the store
+const store = configureStore({
+  reducer:{
+    cart:cartReducer ,
   },
 });
